621d9fa1dc0c5d821cea73fc18206da6618d2239
Yum-git <yzk.yuma@gmail.com>
2020-02-06 22:53:54 +0900

    2020/02/06

--- examples/website/3d-heatmap/app.js
+++ examples/website/3d-heatmap/app.js
@@ -3,14 +3,36 @@ import {render} from 'react-dom';
 import {StaticMap} from 'react-map-gl';
 import {AmbientLight, PointLight, LightingEffect} from '@deck.gl/core';
 import {HexagonLayer} from '@deck.gl/aggregation-layers';
+import {IconLayer} from '@deck.gl/layers';
 import DeckGL from '@deck.gl/react';
 
+var watch_id = navigator.geolocation.watchPosition(test2, function(e) { alert(e.message); }, {"enableHighAccuracy": true, "timeout": 20000, "maximumAge": 2000});
+var iconDatas = [];
+
+const ICON_MAPPING = {
+  marker: {x: 0, y: 0, width: 32, height: 32, mask: true}
+};
+
+function test2(position) {
+    var location = position.coords.longitude + ',' + position.coords.latitude;
+
+    // IconLayer用の形式変換
+    iconDatas =[
+      {
+        position: [position.coords.longitude, position.coords.latitude]
+      }
+    ];
+
+    return location;
+
+}
+
 // Set your mapbox token here
-const MAPBOX_TOKEN = process.env.MapboxAccessToken; // eslint-disable-line
+const MAPBOX_TOKEN = "pk.eyJ1IjoieXVtbmlrb25pa28iLCJhIjoiY2s2MzdhczF6MDdlODNtbjB3NzkydzJ6NSJ9.xdMakcrqSAWMmZMYnQiEYw"; // eslint-disable-line
 
 // Source data CSV
 const DATA_URL =
-  'https://raw.githubusercontent.com/uber-common/deck.gl-data/master/examples/3d-heatmap/heatmap-data.csv'; // eslint-disable-line
+  'Sample_Center.csv'; // eslint-disable-line
 
 const ambientLight = new AmbientLight({
   color: [255, 255, 255],
@@ -39,8 +61,8 @@ const material = {
 };
 
 const INITIAL_VIEW_STATE = {
-  longitude: -1.4157267858730052,
-  latitude: 52.232395363869415,
+  longitude: 137.322986,
+  latitude: 34.82552403,
   zoom: 6.6,
   minZoom: 5,
   maxZoom: 15,
@@ -57,7 +79,7 @@ const colorRange = [
   [209, 55, 78]
 ];
 
-const elevationScale = {min: 1, max: 50};
+const elevationScale = {min: 1, max: 200};
 
 /* eslint-disable react/no-deprecated */
 export default class App extends Component {
@@ -73,15 +95,16 @@ export default class App extends Component {
   }
 
   _renderLayers() {
-    const {data, radius = 1000, upperPercentile = 100, coverage = 1} = this.props;
+    const {data, radius = 400, upperPercentile = 100, coverage = 1} = this.props;
 
+    
     return [
       new HexagonLayer({
         id: 'heatmap',
         colorRange,
         coverage,
         data,
-        elevationRange: [0, 3000],
+        elevationRange: [0, 300],
         elevationScale: data && data.length ? 50 : 0,
         extruded: true,
         getPosition: d => d,
@@ -94,6 +117,23 @@ export default class App extends Component {
         transitions: {
           elevationScale: 3000
         }
+      }),
+      // TODO:
+      new IconLayer({
+        id: 'icon-layer',
+        data: iconDatas,
+        pickable: true,
+        iconAtlas: 'map_pin.png',
+        iconMapping: ICON_MAPPING,
+        getIcon: (iconData) => {
+
+          return "marker";
+        },
+        sizeScale: 15,
+        getPosition: (iconData) => {
+          return iconData.position;
+        },
+        getSize: d => 5
       })
     ];
   }
@@ -128,4 +168,32 @@ export function renderToDOM(container) {
       render(<App data={data} />, container);
     }
   });
+}
+
+// const ICON_MAPPING = {
+//   marker: {x: 0, y: 0, width: 32, height: 32, mask: true}
+// };
+
+const loadData = () =>{
+  data = test2();
+  GPSLy(data);
+}
+const GPSLy = (data) => {
+  const IconLayer = new dack.IconLayer({
+    id: 'icon-layer',
+    data,
+    pickable: true,
+    iconAtlas: 'map-pin.png',
+    iconMapping: ICON_MAPPING,
+    getIcon: d => 'marker',
+    sizeScale: 15,
+    getPosition: data,
+    getSize: d => 5
+  });
+
+  DeckGL.setProps({
+    layers: [IconLayer]
+  });
+
+  renderLayer(data);
 }
